e0cacf1b0e96ced45aef3438c688648d0787bb37
acavaret <atcavaret@gmail.com>
2015-04-08 19:44:44 -0400

    changed foo code

--- foo.js
+++ foo.js
@@ -1,15 +1,15 @@
-app.set('port', (process.env.PORT || 5000));
-
+var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
-var studentCount = 15;
-var className ="Large Systems"
+var server = app.listen(process.env.PORT || 5000, function () {
 
-function greetClass(n, c) {
-console.log("Hello," + n + ", there are " + c + " of you!")
-}
+  var host = server.address().address;
+  var port = server.address().port;
 
-greetClass(className, studentCount);
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
