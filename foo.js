71659f323adf52db776e36e1581f775adb8a5b25
acavaret <atcavaret@gmail.com>
2015-04-07 23:19:11 -0400

    testing connection

---
+++ foo.js
@@ -0,0 +1,8 @@
+var studentCount = 15;
+var className ="Large Systems"
+
+function greetClass(n, c) {
+console.log("Hello," + n + ", there are " + c + " of you!")
+}
+
+greetClass(className, studentCount);
