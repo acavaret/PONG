8648ebad398483ac40f530118b1ac9bd1565102e
acavaret <atcavaret@gmail.com>
2015-04-08 21:37:26 -0400

    redis demo

--- foo.js
+++ foo.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
+var express = require('redis');
+var client = redis.createClient();
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello ' + client.get("hello"));
 });
 
 var server = app.listen(process.env.PORT || 5000, function () {
